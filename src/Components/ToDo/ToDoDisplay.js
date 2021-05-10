57691211a312e7743c64830e8b2a2711acf20aa1
TroySandy <troysandy16@gmail.com>
2021-05-09 21:46:07 -0400

    ready to submit

--- src/Components/ToDo/ToDoDisplay.js
+++ src/Components/ToDo/ToDoDisplay.js
@@ -4,13 +4,14 @@ export default class ToDoDisplay extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      toDoList: [],
+      toDoList: ['get groceries', 'feed dog', 'eat breakfast'],
+      DoneList: []
     };
   }
 
   saveInput = (e) => {
     // console.log('save input');
-    this.setState({ input: e.target.value});
+    this.setState({ input: e.target.value });
   };
 
   addNewItem = (e) => {
@@ -20,24 +21,24 @@ export default class ToDoDisplay extends React.Component {
       toDoList: [...prevState.toDoList, prevState.input],
     }));
   };
-
+ 
   markItemAsComplete = (t) => {
-    // console.log('update');
+      // console.log('update');
     this.setState((state) => {
       const toDoList = state.toDoList.map((item, c) => {
         if (c === t) {
-          // this.setState({completed: true})
-          return item + " Item is completed";
+          return item + ' Item is Completed' ;
         } else {
           return item;
         }
       });
-      console.log(toDoList);
+      return {
+        toDoList,
+      };
     });
   };
 
   render() {
-    console.log(this.state.toDoList);
     return (
       <div>
         <form>
@@ -50,7 +51,7 @@ export default class ToDoDisplay extends React.Component {
           {this.state.toDoList.map((ToDo, index) => {
             return (
               <li key={ToDo}>
-                {ToDo}
+                {ToDo}{index}
                 <button onClick={() => this.markItemAsComplete(index)}>
                   Mark as Complete
                 </button>
